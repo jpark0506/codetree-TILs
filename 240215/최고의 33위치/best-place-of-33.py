ea17c0fd42421a7e8a751147ecbb8e89925b5e93
JunhyeokPark(박준혁) <peterp1@inha.edu>
2024-02-15 00:49:39 +0900

    Title: 최고의 33위치, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240215/최고의 33위치/best-place-of-33.py
+++ 240215/최고의 33위치/best-place-of-33.py
@@ -1,14 +1,18 @@
 n = int(input())
-data = [list(data)for _ in range(n)]
+data = [list(map(int,input().split())) for _ in range(n)]
 max_coin = 0
 def check(coor):
+    global max_coin
     cnt_coin = 0
     x,y = coor[0],coor[1]
-    if x+2<n and y+2 <n:
+    if x+2<n and y+2 < n:
         for i in range(0,3):
             for j in range(0,3):
                 if data[i][j] == 1:
                     cnt_coin += 1
     if max_coin < cnt_coin:
         max_coin = cnt_coin
-print(cnt_coin)
+for i in range(n):
+    for j in range(n):
+        check([i,j])
+print(max_coin)
