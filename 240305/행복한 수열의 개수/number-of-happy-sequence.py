f071fc241deed7b844c167b0798019e91c562abf
JunhyeokPark(박준혁) <peterp1@inha.edu>
2024-03-05 10:41:38 +0900

    Title: 행복한 수열의 개수, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240305/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,32 @@
+m,n = map(int,input().split())
+data = [list(map(int,input().split())) for i in range(n)]
+happy_arr = 0
+#m이 행복함의 기준
+for i in range(n):
+    ref_x = data[i][0]
+    cnt_x = 1
+    for j in range(n):
+        if data[i][j] != ref_x:
+            ref_x = data[i][j]
+            cnt_x = 1
+        else:
+            cnt_x += 1
+            if cnt_x == m :
+                print(i,j)
+                happy_arr += 1
+                break
+       
+for i in range(n):
+    ref_y = data[0][i]
+    cnt_y = 1
+    for j in range(n):
+        if data[j][i] != ref_y:
+            ref_y = data[j][i]
+            cnt_y = 1
+        else:
+            cnt_y += 1
+            if cnt_y == m:
+                print(i,j)
+                happy_arr += 1
+                break
+print(happy_arr)
