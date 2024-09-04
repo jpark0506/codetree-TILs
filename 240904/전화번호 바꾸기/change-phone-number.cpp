5a8cb0456b7392e3aedb9adfbe7144f092d58fd6
JunhyeokPark(박준혁) <peterp1@inha.edu>
2024-09-04 16:49:21 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240904/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char k, dash;
+    int x, y;
+    cin >> k >> dash >> x >> dash >> y;
+    cout << k << dash << y << dash << x;
+    return 0;
+}
