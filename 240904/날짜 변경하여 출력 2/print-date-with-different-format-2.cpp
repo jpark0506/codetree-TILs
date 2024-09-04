2f463c4b712f7f53d93c503a6e626d56a900e32d
JunhyeokPark(박준혁) <peterp1@inha.edu>
2024-09-04 16:47:56 +0900

    Title: 날짜 변경하여 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240904/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y,m,d;
+    char j;
+    char dot = '.';
+    cin >> m >> j >> d >> j >> y;
+    cout << y << dot << m << dot << d;
+    return 0;
+}
