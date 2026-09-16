f66c3143a9671802f6346a22f65d9fce3f5c3a6d
Junhyeok Park(박준혁) <phobipark506@gmail.com>
2026-09-17 08:24:11 +0900

    [Compile Error] 두 개의 격자 비교하기

---
+++ trail1/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N, M;
+    cin >> N >> M;
+
+    int arr1[N][M];
+    int arr2[N][M];
+
+    int ans[N][M];
+
+    for(int i=0; i<2; i++){
+        for(int j = 0; j< N; j++){
+            for(int k = 0; k < M; k++)[
+                int temp;
+                cin >> temp;
+                if(i == 0){
+                    arr1[j][k] = temp;
+                }else{
+                    arr2[j][k] = temp;
+                    if(arr1[j][k] == temp){
+                        ans[j][k] = 1;
+                    }
+                    else{
+                        ans[j][k] = 0;
+                    }
+
+                }
+            ]
+        }
+    }
+
+
+
+    return 0;
+}
