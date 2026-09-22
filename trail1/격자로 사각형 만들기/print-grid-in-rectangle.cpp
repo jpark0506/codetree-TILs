e1532fc618e91af88411dabe0e33030199a544d5
Junhyeok Park(박준혁) <phobipark506@gmail.com>
2026-09-23 07:33:14 +0900

    [Passed] 격자로 사각형 만들기 (2ms, 0MB)

--- trail1/격자로 사각형 만들기/print-grid-in-rectangle.cpp
+++ trail1/격자로 사각형 만들기/print-grid-in-rectangle.cpp
@@ -15,13 +15,13 @@ int main() {
     }
 
     for(int i=1; i<N; i++){
-        for(j=1; j<N; j++){
+        for(int j=1; j<N; j++){
             arr[i][j] = arr[i-1][j-1] + arr[i-1][j] + arr[i][j-1];
         }
     }
 
     for(int i=0; i<N; i++){
-        for(j=0; j<N; j++){
+        for(int j=0; j<N; j++){
             cout << arr[i][j] << " ";
         }
         cout << endl;
