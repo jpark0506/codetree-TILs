8e7981036d6b7ab6be949c7e186c4b529b17ee63
Junhyeok Park(박준혁) <phobipark506@gmail.com>
2026-09-23 07:32:59 +0900

    [Compile Error] 격자로 사각형 만들기

---
+++ trail1/격자로 사각형 만들기/print-grid-in-rectangle.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+
+    cin >> N;
+
+    int arr[N][N];
+
+    for(int i=0; i<N; i++){
+        arr[i][0] = 1;
+        arr[0][i] = 1;
+    }
+
+    for(int i=1; i<N; i++){
+        for(j=1; j<N; j++){
+            arr[i][j] = arr[i-1][j-1] + arr[i-1][j] + arr[i][j-1];
+        }
+    }
+
+    for(int i=0; i<N; i++){
+        for(j=0; j<N; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
