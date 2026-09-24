7a5b61bdf1ff60dff2725a19ddc1fcbbe41ef492
Junhyeok Park(박준혁) <phobipark506@gmail.com>
2026-09-24 16:49:28 +0900

    [Wrong Answer] 양옆 문자 제거하기 (1ms, 0MB)

--- trail1/양옆 문자 제거하기/del-both-side-char.cpp
+++ trail1/양옆 문자 제거하기/del-both-side-char.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 int main() {
     // Please write your code here.
+
+    string str;
+
+    cin >> str;
+
+    str.erase(2,1);
+
+    str.erase(str.length() - 2, 1);
+
+    cout << str;
+
     return 0;
 }
